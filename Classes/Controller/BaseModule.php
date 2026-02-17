CodersCare/l10nmgr

commit 6d9f2a98d365dc396a3320fef29fffd8d09714bc

[BUGFIX] Make sure to fetch SET data from form sent via POST

Classes/Controller/BaseModule.php
@@ -143,7 +143,7 @@ class BaseModule
     {
         $this->MOD_SETTINGS = BackendUtility::getModuleData(
             $this->MOD_MENU,
-            $this->request->getQueryParams()['SET'] ?? '',
+            $this->request->getParsedBody()['SET'] ?? [],
             $this->MCONF['name'],
             $this->modMenu_type,
             $this->modMenu_dontValidateList,

commit e0d9c7f5d81bcaae10a3b47208abd561b5b966b7

[BUGFIX] Use both, GET and POST data to determine current settings
Fixes a regression introduced with 6d9f2a9

Classes/Controller/BaseModule.php
@@ -21,6 +21,7 @@ use Localizationteam\L10nmgr\Traits\BackendUserTrait;
 use Localizationteam\L10nmgr\Traits\LanguageServiceTrait;
 use Psr\Http\Message\ServerRequestInterface;
 use TYPO3\CMS\Backend\Utility\BackendUtility;
+use TYPO3\CMS\Core\Utility\DebugUtility;
 
 /**
  * Used in "BaseScriptClass" from TYPO3 Core
@@ -141,9 +142,10 @@ class BaseModule
      */
     public function menuConfig(): void
     {
+        $setData = array_merge($this->request->getParsedBody()['SET'] ?? [], $this->request->getQueryParams()['SET'] ?? []);
         $this->MOD_SETTINGS = BackendUtility::getModuleData(
             $this->MOD_MENU,
-            $this->request->getParsedBody()['SET'] ?? [],
+            $setData,
             $this->MCONF['name'],
             $this->modMenu_type,
             $this->modMenu_dontValidateList,