a2fa587e8b67dfb779049503d51eda7dddd7bc1d
Jo Hasenau <info@cybercraft.de>
2026-08-25 11:30:53 +0200

    [TASK] Make rootline fields available with CMS 12 again

--- ext_localconf.php
+++ ext_localconf.php
@@ -36,7 +36,7 @@ $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['scheduler']['tasks'][L10nmgrFileGarba
     'additionalFields' => L10nmgrAdditionalFieldProvider::class,
 ];
 
-// $GLOBALS['TYPO3_CONF_VARS']['FE']['addRootLineFields'] .= ',l10nmgr_configuration,l10nmgr_configuration_next_level';
+$GLOBALS['TYPO3_CONF_VARS']['FE']['addRootLineFields'] .= ',l10nmgr_configuration,l10nmgr_configuration_next_level';
 
 LanguageRestrictionRegistry::getInstance()->registerField(
     'core',
